d02578e7fa21db5fdd70fb1e3434d65f392eab26
evelynatamuk <evelynat05@gmail.com>
2024-10-04 16:11:30 +0300

    feat(GameCard.tsx, index.ts): create gamecard component

---
+++ client/src/components/GameCard/Gamecard.tsx
@@ -0,0 +1,7 @@
+import { FC } from "react";
+
+interface GameCardProps {}
+
+export const GameCard: FC<GameCardProps> = ({}) => {
+  return <></>;
+};
--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -1,2 +1,3 @@
 export * from "./ShuffleButton";
 export * from "./Navbar";
+export * from "./GameCard";
