95055ea5e55765cf0e490012b201bf3f149d734b
evelynatamuk <evelynat05@gmail.com>
2024-10-06 01:13:04 +0300

    feat(Nav): add start game button and effect

--- client/src/components/Nav/Nav.tsx
+++ client/src/components/Nav/Nav.tsx
@@ -1,6 +1,8 @@
 import { FC, Dispatch } from "react";
 import { Navbar } from "react-bootstrap";
 import { ShuffleButton } from "../ShuffleButton";
+import { NavLink } from "react-router-dom";
+import "./Nav.css";
 
 interface NavProps {
   shuffleCards: Dispatch<void>;
@@ -8,19 +10,27 @@ interface NavProps {
 
 export const Nav: FC<NavProps> = ({ shuffleCards }) => {
   return (
-    <Navbar
-      bg="dark"
-      data-bs-theme="dark"
-      className="d-flex justify-content-between p-1"
-    >
-      <img
-        src="../../../public/temp-logo.svg"
-        width="50"
-        height="50"
-        className="mx-2"
-      />
-      MemoMaya
-      <ShuffleButton shuffleCards={shuffleCards} />
+    <Navbar bg="dark" data-bs-theme="dark">
+      <Navbar.Brand href="/">
+        <img
+          src="../../../public/temp-logo.svg"
+          width="50"
+          height="50"
+          className="mx-2"
+        />
+        MemoMaya
+      </Navbar.Brand>
+      <NavLink to="/game" className="nav-link mx-2 nav-link-shine">
+        <Navbar.Text
+          style={{ color: "#d54957", fontWeight: "bold", fontSize: "1.2rem" }}
+        >
+          Start a Game
+        </Navbar.Text>
+      </NavLink>
+      <Navbar.Collapse className="justify-content-end">
+        <ShuffleButton shuffleCards={shuffleCards} />
+        <Navbar.Text className="mx-4">© Evelyn & Maya</Navbar.Text>
+      </Navbar.Collapse>
     </Navbar>
   );
 };
