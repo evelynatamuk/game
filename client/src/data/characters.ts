407c4ab4665bcd5640ab5acc037457891a054c0b
evelynatamuk <evelynat05@gmail.com>
2024-10-06 20:16:43 +0300

    rename(characters.json, characters.ts): make a ts data array

---
+++ client/src/data/characters.ts
@@ -0,0 +1,46 @@
+import { Character } from "../types";
+
+const characters: Character[] = [
+  {
+    id: 0,
+    name: "bingo",
+    img: "/bingo.png",
+  },
+  {
+    id: 1,
+    name: "birthday",
+    img: "/birthday.png",
+  },
+  {
+    id: 2,
+    name: "credit",
+    img: "/credit.png",
+  },
+  {
+    id: 3,
+    name: "game",
+    img: "/game.png",
+  },
+  {
+    id: 4,
+    name: "love",
+    img: "/love.png",
+  },
+  {
+    id: 5,
+    name: "red",
+    img: "/red.png",
+  },
+  {
+    id: 6,
+    name: "sim",
+    img: "/sim.png",
+  },
+  {
+    id: 7,
+    name: "tarot",
+    img: "/tarot.png",
+  },
+];
+
+export default characters;
