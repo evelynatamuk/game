24e99d36c8c4ff2a67f870b024717c58555d8d67
evelynatamuk <evelynat05@gmail.com>
2024-10-06 20:14:43 +0300

    feat(App.tsx): wrap app with cards context

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,36 +1,19 @@
-import { useState, useEffect } from "react";
-import characters from "./data/characters.json";
-import Card from "./types/Card";
 import { Nav } from "./components";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import routes from "./routes/routes";
+import { CardsProvider } from "./contexts/Cards";
 
 export const App = () => {
-  const [cards, setCards] = useState<Card[]>([]);
-
-  useEffect(() => {
-    shuffleCards();
-  }, []);
-
-  const shuffleCards = () => {
-    const cards: Card[] = [...characters, ...characters]
-      .sort(() => Math.random() - 0.5)
-      .map((card, index) => {
-        return { id: index, ...card };
-      });
-    setCards(cards);
-  };
-
   return (
-    <>
+    <CardsProvider>
       <BrowserRouter>
-      <Nav shuffleCards={shuffleCards} />
+        <Nav />
         <Routes>
           {routes.map((route, index) => (
             <Route key={index} path={route.path} element={route.element} />
           ))}
         </Routes>
       </BrowserRouter>
-    </>
+    </CardsProvider>
   );
 };
