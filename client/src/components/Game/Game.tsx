b67c25aacaa0cee9a4ec3bc9d4fb05c1caeadf44
evelynatamuk <evelynat05@gmail.com>
2024-10-07 22:42:10 +0300

    style(Game.tsx): add effect on mount

--- client/src/components/Game/Game.tsx
+++ client/src/components/Game/Game.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { FC, useState, useEffect } from "react";
 import { GameCard } from "../GameCard";
 import { useCards } from "../../contexts/Cards";
 import { Container, Row, Col } from "react-bootstrap";
@@ -8,8 +8,22 @@ interface GameProps {}
 export const Game: FC<GameProps> = () => {
   const { cards } = useCards();
 
+  const [opacity, setOpacity] = useState(0);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setOpacity(1);
+    }, 300);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <Container fluid className="col-6">
+    <Container
+      fluid
+      className="col-6"
+      style={{ opacity, transition: "opacity 0.5s ease-in-out" }}
+    >
       <Row className="justify-content-center">
         {cards.map((card) => (
           <Col key={card.cardId} className="col-3 mt-1">
